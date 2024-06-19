194ef9d70f71499923fa3eb0cc4b7d756abb62b8
KAUTSAR <kautsarmachine2@gmail.com>
2024-06-19 23:22:37 +0700

    week 7 belum model

--- lib/app/data/API/presence_api.dart
+++ lib/app/data/API/presence_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:intl/intl.dart';
 import 'package:project_absensi/app/data/api_client.dart';
 
 class PresenceApi extends ApiClient {
@@ -9,13 +10,16 @@ class PresenceApi extends ApiClient {
     required String waktuAbsenMasuk,
     }) async {
     try {
+      final DateTime now = DateTime.now();
+      final DateFormat formatter = DateFormat('yyyy-MM-dd');
+      final String formatted = formatter.format(now);
       var response = await dio.post(
         '$baseUrl/pegawai/absen',
         data: {
           'users_id' : usersId,
           'lokasi_user' : lokasi,
           'waktu_absen_masuk' : waktuAbsenMasuk,
-          'tanggal_hari_ini' : DateTime.now(),
+          'tanggal_hari_ini' : formatted,
         },
         options: Options(
           contentType: Headers.formUrlEncodedContentType,
@@ -29,4 +33,49 @@ class PresenceApi extends ApiClient {
       return e.response!;
     }
   }
+
+  Future<Response> cekAbsenHAriIni({
+    required String accesstoken,
+    required String usersId,
+    required String waktuHariIni,
+    }) async {
+    try {
+      var response = await dio.get(
+        '$baseUrl/cek_absen_hari_ini/$usersId/$waktuHariIni',
+
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {
+            'Authorization': 'Bearer $accesstoken',
+          },
+        ),
+      );
+      return response;
+    } on DioException catch (e) {
+      return e.response!;
+    }
+  }
+
+  Future<Response> absenHistory({
+    required String accesstoken,
+    required String usersId,
+    
+    }) async {
+    try {
+      var response = await dio.get(
+        '$baseUrl/absen_history/$usersId',
+
+        options: Options(
+          contentType: Headers.formUrlEncodedContentType,
+          headers: {
+            'Authorization': 'Bearer $accesstoken',
+          },
+        ),
+      );
+      return response;
+    } on DioException catch (e) {
+      return e.response!;
+    }
+  }
+
 }
--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,224 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:maps_launcher/maps_launcher.dart';
+import 'package:project_absensi/app/data/API/controller/auth_controller.dart';
+import 'package:project_absensi/app/data/API/presence_api.dart';
+import 'package:project_absensi/app/data/API/profile_api.dart';
+import 'package:project_absensi/app/modules/profile/controllers/profile_model.dart';
+import 'package:project_absensi/app/routes/app_pages.dart';
+import 'package:project_absensi/app/widget/toast/custom_toast.dart';
 
+// class HomeController extends GetxController {
+//   //TODO: Implement HomeController
+
+//   final count = 0.obs;
+//   @override
+//   void onInit() {
+//     super.onInit();
+//   }
+
+//   @override
+//   void onReady() {
+//     super.onReady();
+//   }
+
+//   @override
+//   void onClose() {
+//     super.onClose();
+//   }
+
+//   void increment() => count.value++;
+// }
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final authC = Get.find<AuthController>();
+  RxBool isLoading = false.obs;
+  RxString officeDistance = "-".obs;
+  Timer? timer;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    //cek setiap 10 detik lokasi posisinya dimana
+    timer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      if (Get.currentRoute == Routes.HOME) {
+        getDistanceToOffice().then((value) {
+          officeDistance.value = value;
+        });
+      }
+    });
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  launchOfficeOnMap() {
+    try {
+      MapsLauncher.launchCoordinates(
+        double.parse(authC.profilPerusahaanModel.data!.latitude!),
+        double.parse(authC.profilPerusahaanModel.data!.longitude!),
+      );
+    } catch (e) {
+      CustomToast.errorToast('Error', 'Error : $e');
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  Future<String> getDistanceToOffice() async {
+    Map<String, dynamic> determinePosition = await _determinePosition();
+    if (!determinePosition["error"]) {
+      Position position = determinePosition["position"];
+      double distance = Geolocator.distanceBetween(
+        double.parse(authC.profilPerusahaanModel.data!.latitude!),
+        double.parse(authC.profilPerusahaanModel.data!.longitude!),
+        position.latitude,
+        position.longitude,
+      );
+      if (distance > 1000) {
+        return "${(distance / 1000).toStringAsFixed(2)}km";
+      } else {
+        return "${distance.toStringAsFixed(2)}m";
+      }
+    } else {
+      return "-";
+    }
   }
 
-  void increment() => count.value++;
+  Future<Map<String, dynamic>> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      Get.rawSnackbar(
+        title: 'GPS is off',
+        message: 'you need to turn on gps',
+        duration: const Duration(seconds: 3),
+      );
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        // return Future.error('Location permissions are denied');
+        return {
+          "message":
+              "Tidak dapat mengakses karena anda menolak permintaan lokasi",
+          "error": true,
+        };
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return {
+        "message":
+            "Location permissions are permanently denied, we cannot request permissions.",
+        "error": true,
+      };
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    return {
+      "position": position,
+      "message": "Berhasil mendapatkan posisi device",
+      "error": false,
+    };
+  }
+
+  ProfileModel profileModel = ProfileModel();
+
+  Future<ProfileModel> getUser() async {
+    try {
+      var res = await ProfileApi().getProfile(
+        accesstoken: authC.currentToken!,
+      );
+      if (res.data['success'] == true) {
+        profileModel = ProfileModel.fromJson(res.data);
+        return profileModel;
+      } else {
+        Get.rawSnackbar(
+          messageText: Text(res.data['message'].toString()),
+          backgroundColor: Colors.red.shade300,
+        );
+        return profileModel;
+      }
+    } catch (e) {
+      Get.rawSnackbar(
+        messageText: Text(e.toString()),
+        backgroundColor: Colors.red.shade300,
+      );
+      return profileModel;
+    }
+  }
+
+  AbsenHariIniModel absenHariIniModel = AbsenHariIniModel();
+
+  Future<AbsenHariIniModel> getCekAbsenHariIni() async {
+    try {
+      final DateTime now = DateTime.now();
+      final DateFormat formatter = DateFormat('yyyy-MM-dd');
+      final String formatted = formatter.format(now);
+      var res = await PresenceApi().cekAbsenHariIni(
+        accesstoken: authC.currentToken!,
+        usersId: authC.currentUsersId!,
+        waktuHariIni: formatted,
+      );
+      if (res.data['success'] == true) {
+        absenHariIniModel = AbsenHariIniModel.fromJson(res.data);
+        return absenHariIniModel;
+      } else {
+        Get.rawSnackbar(
+          messageText: Text(res.data['message'].toString()),
+          backgroundColor: Colors.red.shade300,
+        );
+        return absenHariIniModel;
+      }
+    } catch (e) {
+      Get.rawSnackbar(
+        messageText: Text(e.toString()),
+        backgroundColor: Colors.red.shade300,
+      );
+      return absenHariIniModel;
+    }
+  }
+
+  AbsenHistoryModel absenHistoryModel = AbsenHistoryModel();
+  Future<AbsenHistoryModel> getAbsenHistory() async {
+    try {
+      var res = await PresenceApi().absenHistory(
+        accesstoken: authC.currentToken!,
+        usersId: authC.currentUsersId!,
+      );
+      if (res.data['success'] == true) {
+        absenHistoryModel = AbsenHistoryModel.fromJson(res.data);
+        return absenHistoryModel;
+      } else {
+        Get.rawSnackbar(
+          messageText: Text(res.data['message'].toString()),
+          backgroundColor: Colors.red.shade300,
+        );
+        return absenHistoryModel;
+      }
+    } catch (e) {
+      Get.rawSnackbar(
+        messageText: Text(e.toString()),
+        backgroundColor: Colors.red.shade300,
+      );
+      return absenHistoryModel;
+    }
+  }
 }
